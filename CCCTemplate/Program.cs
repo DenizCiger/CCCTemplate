4eb69b49671efd1414790779cc52c1d35cc4072d
DenizCiger <deniz.ciger@gmx.at>
2024-10-25 08:26:16 +0200

    Add project files.

---
+++ CCCTemplate/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CCCTemplate
+{
+    internal class Program
+    {
+        public const int LEVEL = 1;
+        static void Main(string[] args)
+        {
+            string[][] inputs = Util.GetInputs(LEVEL);
+            List<string[]> outputs = new List<string[]>();
+
+            HandleInputs(inputs, outputs);
+
+            Util.GenerateOutputs(LEVEL, outputs.ToArray());
+            bool exampleOutEquals = Util.CompareExampleOutEquals(LEVEL, $"level{LEVEL}_example.out");
+        }
+        private static void HandleInputs(string[][] inputs, List<string[]> outputs)
+        {
+            switch (LEVEL)
+            {
+                case 1:
+                    foreach (string[] input in inputs)
+                    {
+                        List<string> output = new List<string>();
+
+                        // Implement logic here
+                        // Add all the output lines of a single input file to the output list
+                        
+
+
+                        // Ouput of all files are then stored in the outputs list
+                        outputs.Add(output.ToArray());
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Invalid level");
+            }
+        }
+    }
+}
---
+++ CCCTemplate/Util.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCCTemplate
+{
+    internal class Util
+    {
+
+        const string OUTPUT_DIR = "output";
+        public static string[][] GetInputs(int level, string path = "./")
+        {
+            Console.WriteLine("Starting reading of inputs...");
+            string dirname = $"{path}level{level}";
+
+            if (!Directory.Exists(dirname))
+            {
+                throw new ArgumentException($"Directory {dirname} does not exist.");
+            }
+
+            string[] fileNames = Directory.GetFiles(dirname, "*.in");
+
+            string[][] inputs = new string[fileNames.Length][];
+
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                inputs[i] = File.ReadAllLines(fileNames[i]);
+            }
+
+            Console.WriteLine("Completed reading inputs!");
+            return inputs;
+        }
+
+        public static void GenerateOutputs(int level, string[][] outputContent, string path = "./")
+        {
+            Console.WriteLine("Starting generation of output files...");
+            string dirname = $"{path}level{level}";
+
+            if (!Directory.Exists(dirname))
+            {
+                throw new ArgumentException($"Directory {dirname} does not exist.");
+            }
+
+            if (!Directory.Exists($"{dirname}/{OUTPUT_DIR}"))
+            {
+                Directory.CreateDirectory($"{dirname}/{OUTPUT_DIR}");
+            }
+
+            string[] inFileNames = Directory.GetFiles(dirname, "*.in");
+            string[] outFileNames = inFileNames.Select(f => Path.Combine(Path.GetDirectoryName(f), OUTPUT_DIR, Path.GetFileNameWithoutExtension(f) + ".out")).ToArray();
+
+            for (int i = 0; i < outFileNames.Length; i++)
+            {
+                File.WriteAllLines(outFileNames[i], outputContent[i]);
+            }
+            Console.WriteLine("Output files generated!");
+        }
+
+        public static bool CompareExampleOutEquals(int level, string fileName, string path = "./")
+        {
+            string dirname = $"{path}level{level}";
+
+            if (!Directory.Exists(dirname))
+            {
+                Console.WriteLine($"Directory {dirname} does not exist.");
+                return false;
+            }
+
+            if (File.Exists($"{dirname}/{fileName}") && File.Exists($"{dirname}/{OUTPUT_DIR}/{fileName}"))
+            {
+                bool equals = File.ReadAllText($"{dirname}/{fileName}") == File.ReadAllText($"{dirname}/{OUTPUT_DIR}/{fileName}");
+                Console.ForegroundColor = equals ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.Write("Example output files are " + (!equals ? "not " : "") + "equals");
+                Console.ResetColor();
+                return equals;
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} or {fileName.Replace(".in", ".out")} does not exist.");
+                return false;
+            }
+
+        }
+    }
+}
